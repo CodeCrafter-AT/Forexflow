91c7fe52862d3dd17b71783c358c7e663ecfcb59
Arpit Tripathi <thearpit637@gmail.com>
2026-03-27 14:47:31 +0530

    Fix vite config syntax error

--- forexflow-frontend/vite.config.js
+++ forexflow-frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
+  
   return {
     plugins: [react()],
     server: {
@@ -14,4 +15,5 @@ export default defineConfig(({ mode }) => {
         }
       }
     }
-})
+  };
+});
